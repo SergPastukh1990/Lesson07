0ae0d7f1b8142047292f2a8f4b6f3376c879912d
SergPastukh1990 <57325363+SergPastukh1990@users.noreply.github.com>
2019-11-24 21:54:40 +0200

    Update Lesson7_hw03.py

--- Lesson7_hw03.py
+++ Lesson7_hw03.py
@@ -2,10 +2,10 @@
 # Добавьте символ перехода на новую строку \n в конце каждой строки при добавлении в файл.
 # f = open  ('/Users/sergiipastukh/Desktop/python_course/Lesson 5/persons1.txt', 'x')
 # f.close()
-with open('/Users/sergiipastukh/Desktop/python_course/Lesson 5/persons1.txt', 'w') as f:
+with open('persons1.txt', 'w') as f:
     f.write("harry potter; 30; 127.45 galeon; aurorn\nross geller; 34; 6500.45 usd; paleontologist\n")
 
-with open('/Users/sergiipastukh/Desktop/python_course/Lesson 5/persons1.txt') as f:
+with open('persons1.txt') as f:
     print("single line:", f.readlines())
 
 
